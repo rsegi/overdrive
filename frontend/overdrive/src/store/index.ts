34b0935da9544aa78dcde1d949bb7ab82c9630cb
Raul Segui <r.segui.t@gmail.com>
2021-10-30 22:49:13 +0200

    add funtionality to vuex

--- frontend/overdrive/src/store/index.ts
+++ frontend/overdrive/src/store/index.ts
@@ -1,6 +1,17 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import { Product } from "@/models/product";
 
-export default createStore({
+export interface State {
+  cart: {
+    items: Product[]
+  },
+  isAuthenticated: boolean,
+  token: string,
+  isLoading: boolean,
+}
+
+
+export default createStore<State>({
   state: {
     cart: {
       items: [],
@@ -10,6 +21,22 @@ export default createStore({
     isLoading: false,
   },
   mutations: {
+    initializeStore(state) {
+      if (localStorage.getItem('cart')){
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        state.cart = JSON.parse(localStorage.getItem('cart')!);
+      } else {
+        localStorage.setItem('cart', JSON.stringify(state.cart));
+      }
+    },
+    addToCart(state, item){
+      const existingItems = state.cart.items.filter(i => i.id === item.product.id)
+      if (existingItems.length) {
+        existingItems[0].quantity = existingItems[0].quantity + parseInt(item.quantity)
+      } else {
+        state.cart.items.push(item)
+      }
+    }
   },
   actions: {
   },
