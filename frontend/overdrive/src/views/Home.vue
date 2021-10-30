9923865b98800131bb0c9f0e54bb0a5017a6ff8a
Raul Segui <r.segui.t@gmail.com>
2021-10-30 20:23:42 +0200

    show all products on home page

--- frontend/overdrive/src/views/Home.vue
+++ frontend/overdrive/src/views/Home.vue
@@ -10,42 +10,50 @@
 
     <div class="columns is-multiline">
       <div class="column is-12">
-          <h2 class="is-size-2 has-text-centered">Latest products</h2>
+          <h2 class="is-size-2 has-text-centered">Products</h2>
       </div>
 
       <div 
         class="column is-3"
-        v-for="product in latestProducts"
+        v-for="product in products"
         v-bind:key="product.id"
       >
+       <div class="box">
+            <figure class="image mb-4">
+                <img v-bind:src="product.image">
+            </figure>
+
+            <h3 class="is-size-4">{{ product.name }}</h3>
+            <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+
+            <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-import axios from 'axios';
-// @ is an alias to /src
+import productService from '@/services/productService';
+import {Product} from 'src/models/product';
 
 export default {
   name: 'Home',
-  data():string[] {
-    return {
-      latestProducts: []
-    }
+  data(): Product[] {
+    return [] // TODO: return products
+    
   },
   components: {
   },
   mounted(): void {
-    this.getLatestProducts()
+    this.getProducts()
   },
   methods: {
-    getLatestProducts(){
-      axios.get('api/v1/latest-products/')
-      .then(response => {
-        this.latestProducts = response.data
+    getProducts(): void {
+      productService.getProducts().then(response => {
+        this.products = response.data;
+      }).catch((error: Error) => {
+        console.log(error);
+        
       })
-      .catch(error => {
-          console.log(error)
-        })
     }
   }
   }
