da82997ece35226ff9b682edc85b3c1ba1bb675e
Raul Segui <r.segui.t@gmail.com>
2021-10-30 20:23:22 +0200

    implement product service

--- frontend/overdrive/src/services/productService.ts
+++ frontend/overdrive/src/services/productService.ts
@@ -1,9 +1,14 @@
+import { Product } from "@/models/product";
 import { AxiosResponse } from "axios";
 import http from "./common/http-common";
 
 class ProductService {
-  getProducts(): Promise<AxiosResponse<string>> {
-    return http.get("/");
+  getProducts(): Promise<AxiosResponse<Product[]>> {
+    return http.get("/products"); // TODO: use correct endpoint
+  }
+  
+  getProduct(category: string, productId:string): Promise<AxiosResponse<Product>> {
+    return http.get(`/${category}/${productId}`); // TODO: use correct endpoint
   }
 }
 
