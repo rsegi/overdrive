a1cef2dd9583512f0713f2a33b661e725b803762
Raul Segui <r.segui.t@gmail.com>
2021-11-14 22:59:07 +0100

    Add category controller + dto

--- api/dtos/categoryDto.ts
+++ api/dtos/categoryDto.ts
@@ -1,5 +1,26 @@
+import ProductDto, { getProductDto } from "./productDto";
+
 export default interface CategoryDto {
   id: string;
   name: string;
   image: string;
+  products: ProductDto[];
 }
+
+export const getCategoryDto = (c: any) => {
+  const { id, image, name, Products } = c.dataValues;
+
+  const products: ProductDto[] = Products.map((p: any) => {
+    const product = getProductDto(p);
+
+    return product;
+  });
+
+  const category: CategoryDto = {
+    id: id,
+    image: image,
+    name: name,
+    products: products,
+  };
+  return category;
+};
