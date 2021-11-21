f0a0c2031e75730a71bbd4ded42029ad867afb76
Raul Segui <r.segui.t@gmail.com>
2021-11-21 23:39:08 +0100

    add images

--- frontend/overdrive/src/components/CategoryCard.vue
+++ frontend/overdrive/src/components/CategoryCard.vue
@@ -1,13 +1,12 @@
 <template>
-    <div class="is-dark column is-3">
+    <div class="is-dark column">
       <router-link v-bind:to="'/categories/' + category.id">
         <div class="box">
               <figure class="image mb-4">
-                  <img v-bind:src="category.image">
+                  <img v-bind:src="require('@/assets/images/' + category.image)">
               </figure>
 
               <h3 class="is-size-4 has-text-centered">{{ category.name }}</h3>
-              
           </div>
         </router-link>
       </div>
--- frontend/overdrive/src/components/ProductCard.vue
+++ frontend/overdrive/src/components/ProductCard.vue
@@ -3,11 +3,11 @@
       <router-link v-bind:to="'/products/' + product.id">
         <div class="box">
           <figure class="image mb-4">
-              <img v-bind:src="product.image">
+              <img v-bind:src="require('@/assets/images/' + product.image)">
           </figure>
 
           <h3 class="is-size-4">{{ product.name }}</h3>
-          <p class="is-size-6 has-text-grey">${{ product.price }}</p>
+          <p class="is-size-6 has-text-grey">{{ product.price }} €</p>
 
           </div>
       </router-link> 
