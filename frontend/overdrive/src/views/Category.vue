35074cd39dd470f903d352c9909b3bbab9e05116
Raul Segui <r.segui.t@gmail.com>
2021-11-21 22:55:30 +0100

    set right lifecycle for component call

--- frontend/overdrive/src/views/Category.vue
+++ frontend/overdrive/src/views/Category.vue
@@ -1,62 +1,61 @@
 <template>
   <div class="page-category">
-     <div class="columns is-multiline">
-            <div class="column is-12">
-                <h2 class="is-size-2 has-text-centered">{{ categoryName }}</h2>
-            </div>
+    <div class="columns is-multiline">
+      <div class="column is-12">
+        <h2 class="is-size-2 has-text-centered">{{ categoryName }}</h2>
+      </div>
 
-      <ProductCard 
+      <ProductCard
         v-for="product in products"
         v-bind:key="product.id"
-        v-bind:product="product" 
+        v-bind:product="product"
       />
-     </div>
+    </div>
   </div>
 </template>
-
 
 <script lang="ts">
 import { IProduct } from "@/models/product";
 import categoryService from "@/services/categoryService";
-import ProductCard from '@/components/ProductCard.vue'
+import ProductCard from "@/components/ProductCard.vue";
 import { defineComponent } from "vue";
 
 interface Data {
-  categoryName: string,
-  products: IProduct[]
+  categoryName: string;
+  products: IProduct[];
 }
 
 export default defineComponent({
-  name: 'Category',
-  components:{
+  name: "Category",
+  components: {
     ProductCard,
   },
-  data():Data { 
+  data(): Data {
     return {
-      categoryName: '',
-      products:[]
-    }
+      categoryName: "",
+      products: [],
+    };
   },
-  mounted() {
+  created() {
     this.getProductsByCategoryId();
   },
   methods: {
     async getProductsByCategoryId(): Promise<void> {
-      this.$store.commit('setIsLoading', true);
+      this.$store.commit("setIsLoading", true);
 
       const categoryId = this.$route.params.categoryId as string;
-      await categoryService.getProductsByCategoryId(categoryId)
-      .then((response) => {
+      await categoryService
+        .getProductsByCategoryId(categoryId)
+        .then((response) => {
           this.products = response.data.products;
           this.categoryName = response.data.name;
 
-          document.title = this.categoryName + ' | Overdrive';
-
+          document.title = this.categoryName + " | Overdrive";
         })
-      .catch((error: Error) => console.log(error));
+        .catch((error: Error) => console.log(error));
 
-      this.$store.commit('setIsLoading', false);
+      this.$store.commit("setIsLoading", false);
     },
-  }, 
+  },
 });
 </script>
--- frontend/overdrive/src/views/Product.vue
+++ frontend/overdrive/src/views/Product.vue
@@ -1,84 +1,84 @@
 <template>
   <div class="page-product">
-          <div class="columns is-multiline">
-              <div class="column is-9">
-                  <figure class="image mb-6">
-                      <img v-bind:src="product.image">
-                  </figure>
-                  
-                  <h1 class="title">{{ product.name }}</h1>
+    <div class="columns is-multiline">
+      <div class="column is-9">
+        <figure class="image mb-6">
+          <img v-bind:src="product.image" />
+        </figure>
 
-                  <p>{{ product.description }}</p>
-              </div>
+        <h1 class="title">{{ product.name }}</h1>
 
-          <div class="column is-3">
-              <h2 class="subtitle">Information</h2>
-
-              <p><strong>Price: </strong>${{ product.price }}</p>
-
-              <div class="field has-addons mt-6">
-                  <div class="control">
-                      <input type="number" class="input" min="1" v-model="quantity">
-                  </div>
-
-                  <div class="control">
-                      <a class="button is-dark" @click="addToCart()">Add to cart</a>
-                  </div>
-              </div>
-          </div>
+        <p>{{ product.description }}</p>
       </div>
+
+      <div class="column is-3">
+        <h2 class="subtitle">Information</h2>
+
+        <p><strong>Price: </strong>${{ product.price }}</p>
+
+        <div class="field has-addons mt-6">
+          <div class="control">
+            <input type="number" class="input" min="1" v-model="quantity" />
+          </div>
+
+          <div class="control">
+            <a class="button is-dark" @click="addToCart()">Add to cart</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-  
 </template>
 
 <script lang="ts">
 import productService from "@/services/productService";
-import { IProduct } from '@/models/product';
+import { IProduct } from "@/models/product";
 import { defineComponent } from "vue";
 
 interface Data {
-   product: IProduct | null,
-   quantity: number,
+  product: IProduct | null;
+  quantity: number;
 }
 
 export default defineComponent({
-  name: 'Product',
-  data():Data { 
-    return{
+  name: "Product",
+  data(): Data {
+    return {
       product: null,
-      quantity: 1
-    }
+      quantity: 1,
+    };
   },
-  mounted() {
+  created() {
     this.getProduct();
   },
   methods: {
     async getProduct(): Promise<void> {
-      this.$store.commit('setIsLoading', true);
+      this.$store.commit("setIsLoading", true);
 
       const productId = this.$route.params.productId as string;
-      await productService.getProduct(productId)
-      .then((response) => {
+      await productService
+        .getProduct(productId)
+        .then((response) => {
           this.product = response.data;
 
-          document.title = this.product.name + ' | Overdrive';
+          document.title = this.product.name + " | Overdrive";
         })
-      .catch((error: Error) => console.log(error));
+        .catch((error: Error) => console.log(error));
 
-      this.$store.commit('setIsLoading', false);
+      this.$store.commit("setIsLoading", false);
     },
 
     addToCart() {
-      if(isNaN(this.quantity) || this.quantity < 1) {
+      if (isNaN(this.quantity) || this.quantity < 1) {
         this.quantity = 1;
       }
       const item = {
         product: this.product,
         quantity: this.quantity,
-      }
+      };
 
-      this.$store.commit('addToCart', item);
-    }
+      this.$store.commit("addToCart", item);
+    },
   },
 });
 </script>
